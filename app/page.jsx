40e54cf2a96f2bdc66cd1e7b094db4cc2d86e74f
ömer turan <omerturandeveloper@gmail.com>
2023-12-18 12:37:12 +0300

    ..

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,26 @@
+import Link from 'next/link'
 
 export default function Home() {
   return (
-  <div>Hello world</div>
-  )
+  <div>
+    <Link href="/hakkimizda">Hakkımızda</Link>
+    <br />
+    <Link href="/docs/post-1" >Post 1</Link>
+    <br />
+    <Link 
+    target='_blank'
+    className='Test'
+    href={{
+      pathname : "/hakkimizda",
+      query: {
+        name: "next",
+        surname: "JS",
+      },
+    }}
+    >
+    
+    Hakkımızda Obje İle
+    </Link>
+  </div>
+  );
 }
